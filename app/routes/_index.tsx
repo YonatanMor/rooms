2d77bdcf173c7032143357fad93a3e3a13323b69
Yoni Mor <yoni304@gmail.com>
2024-07-01 11:58:23 +0300

    1

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,7 +1,7 @@
 export default function Index() {
   return (
-    <div className="h-96 w-96 flex justify-center items-center text-4xl bg-slate-600">
-      Hello World!
+    <div className="h-[100vh] w-full flex justify-center items-center text-4xl bg-slate-600">
+      <h1>Hello World!</h1>
     </div>
   )
 }
