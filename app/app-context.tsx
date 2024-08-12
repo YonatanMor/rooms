92ba5ed425db000b08f0494e9aef4c43dac6db37
Yoni Mor <yoni304@gmail.com>
2024-08-12 16:12:25 +0300

    fix lint

--- app/app-context.tsx
+++ app/app-context.tsx
@@ -1,15 +1,17 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useState } from "react"
 
 export const AppContext = createContext({
   showMenu: false,
+  // eslint-disable-next-line
   setShowMenu: (_show: boolean) => {},
   showEventDialog: false,
+  // eslint-disable-next-line
   setShowEventDialog: (_show: boolean) => {},
-});
+})
 
 export default function ContextProvider({ children }: { children: ReactNode }) {
-  const [showMenu, setShowMenu] = useState<boolean>(false);
-  const [showEventDialog, setShowEventDialog] = useState<boolean>(false);
+  const [showMenu, setShowMenu] = useState<boolean>(false)
+  const [showEventDialog, setShowEventDialog] = useState<boolean>(false)
 
   return (
     <AppContext.Provider
@@ -22,5 +24,5 @@ export default function ContextProvider({ children }: { children: ReactNode }) {
     >
       {children}
     </AppContext.Provider>
-  );
+  )
 }
