696fd6480ae99741334a15284ff4ab47426f4d14
Yoni Mor <yoni304@gmail.com>
2024-08-14 13:46:37 +0300

    change port to 3000

--- server.mjs
+++ server.mjs
@@ -50,7 +50,7 @@ app.use(morgan("tiny"))
 // handle SSR requests
 app.all("*", remixHandler)
 
-const port = process.env.PORT || 5200
+const port = process.env.PORT || 3000
 app.listen(port, () =>
   // eslint-disable-next-line no-console
   console.log(`Express server listening at http://localhost:${port}`),
